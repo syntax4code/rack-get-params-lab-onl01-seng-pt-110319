34b1f2198d640f22e1bbfc2da2f1e56804a6dd37
Warren Harper II <warrenharper2@att.net>
2020-02-10 16:50:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,16 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      end
+      
     else
       resp.write "Path Not Found"
     end
