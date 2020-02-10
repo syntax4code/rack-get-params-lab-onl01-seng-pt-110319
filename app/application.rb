de3f7d08fa530f87f4c595b1d1ae0b082f9d6e87
Warren Harper II <warrenharper2@att.net>
2020-02-10 17:00:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,9 @@ class Application
          if @@items.include?(item)
            @@cart << item
            resp.write "added #{item}"
+         else
+           resp.write "We don't have that item"
+         end
     else
       resp.write "Path Not Found"
     end
